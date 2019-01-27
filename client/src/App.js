5dfe7eb146b5c2e16a80e7d896164da36dff9cd1
abood <tahooo57@gmail.com>
2019-01-27 11:52:56 +0300

    integration with redux and mcreate register page

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,30 @@
 import React, { Component } from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 import './App.css';
 import Navbar from './components/layouts/Navbar.js';
 import Footer from './components/layouts/Footer.js';
 import Landing from './components/layouts/Landing.js';
+import Login from './components/auth/Login.js';
+import Register from './components/auth/Register.js';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar />
-        <Landing />
-        <Footer />
-      </div>
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <Route path="/" component={Landing} exact />
+            <div className="container">
+              <Route path="/login" component={Login} exact />
+              <Route path="/register" component={Register} exact />
+            </div>
+            <Footer />
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }
